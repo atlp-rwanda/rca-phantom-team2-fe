06164e306dd3d4624cc6418061dc39b63dc7b129
Jacques <51080275+jacques-twizeyimana@users.noreply.github.com>
2023-04-21 16:17:55 +0200

    change tech stack to vite and ts  (#7)
    * chore: change techstack to vite, react and ts
    
    * feat: add vscode settings to fix eslint errors on save
    
    * chore: recommend some vscode extensions
    
    * feat: improve template pages UI
    
    * chore: ignore vite build files
    
    * feat: install react redux
    
    * feat: create counter store
    
    * feat: test counter redux store
    
    * feat: test our global state using many components
    
    * feat: add netlify redirect rules
    
    * rf: change typescript configurations
    
    * fix: lint issues
    
    * rf: remove pnpm lock
    
    * rf: remove pnpm lock
    
    * rf: force to use npm as a package manager
    
    * feat: add vite config alias for importing relative url

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from "react";
 import { Provider } from "react-redux";
-import { Routes, Route } from "react-router-dom";
-import { store } from "./store";
-import Home from "./pages/Home";
-import PageNotFound from "./pages/PageNotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Counter from "@/components/Counter";
+import Home from "@/pages/Home";
+import PageNotFound from "@/pages/PageNotFound";
+import { store } from "@/store";
 
 function App() {
   return (
     <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Counter from "../components/Counter";
-
 export default function Home() {
-  return <div className="w-screen h-screen flex flex-col justify-center items-center bg-white text-black">
-    <Counter/>
-  </div>;
+  return (    
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-green-800 font-black text-5xl">Phantom</h1>
+      <img src="/assets/react.svg" className="w-24 h-24 animate-bounce mt-6" />
+    </div>
+  )
 }
