29fde6c4c5b59fabfa8123265a0ba61f26e3b204
Manzi Mike <manzimike378@gmail.com>
2023-04-24 14:20:49 +0200

    fix(ci): resolve failing checks

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 const Toggle = () => {
     const [on, setOn] = useState(false);
     return <button id="btn" onClick={() => setOn(!on)}>{on ? "ON" : "OFF"}</button>
