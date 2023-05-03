29e500d8466ab2bcbf513e54b6dff84a3dd104e2
belladev250 <habanathomas1982@gmail.com>
2023-05-03 15:15:29 +0200

    driver

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound";
+import RegisterDriver from "./components/RegisterDrivers";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<RegisterDriver />} />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/regdriver" element={<Home />} />
       </Routes>
     </div>
   );
---
+++ src/components/RegisterDrivers.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function RegisterDriver() {
+  return (
+    <div className=" bg-gray-100 ">
+      <div className="bg-white  w-1/3 h-5/6"></div>
+      <div className="bg-white w-1/3 h-5/6 ">
+        <form action="inline">
+          <input type="Firstname" />
+          <input type="Lastname" />
+          <input type="email" />
+          <input type="text" />
+          <input type="text" />
+          <input type="button" value="" />
+        </form>
+      </div>
+    </div>
+  );
+}
