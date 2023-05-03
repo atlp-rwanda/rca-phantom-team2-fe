a22c215b41a061be0e91a2279a4b139471556cbe
belladev250 <habanathomas1982@gmail.com>
2023-05-03 18:08:50 +0200

    deleted-home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import RegisterDriver from "./pages/RegisterDriver";
 import PageNotFound from "./pages/PageNotFound";
 import { store } from "./store";
@@ -10,7 +9,6 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/registerDriver" element={<RegisterDriver />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
