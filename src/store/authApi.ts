95caec506c935872db30d0dfbfcd80c5803b7c54
kelliaUmuhire <kellumuhire@gmail.com>
2023-05-09 10:12:16 -0700

    feat(signIn): adjust styles

--- src/store/authApi.ts
+++ src/store/authApi.ts
@@ -1,16 +1,18 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import baseUrl from '../utils/url';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseUrl from "../utils/url";
 
 export const login = createAsyncThunk(
-  'auth/login',
-  async (payload: { email: string; password: string, device_id: string }, thunkAPI) => {
+  "auth/login",
+  async (
+    payload: { email: string; password: string; device_id: string },
+    thunkAPI
+  ) => {
     try {
       const { data } = await axios.post(`${baseUrl}/users/signin`, payload);
-      console.log(data);
       return data;
-    // rome-ignore lint/suspicious/noExplicitAny: <explanation>
-}  catch (error: any) {
+      // rome-ignore lint/suspicious/noExplicitAny: <explanation>
+    } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data);
     }
   }
